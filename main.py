7bc537d28e917b8e9103d1ab514366b916b93088
Nuhirath Rafthia <nr2793@columbia.edu>
2023-12-19 18:03:44 -0500

    Add description, image, and links for each project title

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ Below you can find some of the apps I have built in Python. Feel free to contact
 """
 st.write(content2)
 
-col3, col4 = st.columns(2)
+col3, empty_col, col4 = st.columns([1.5, 0.5, 1.5])
 
 df = pandas.read_csv("data.csv", sep=";")
 
@@ -33,8 +33,15 @@ with col3:
     for index, row in df.iterrows():
         if index % 2 == 0:
             st.header(row["title"])
+            st.write(row["description"])
+            st.image("images/" + row["image"])
+            st.write(f"[Source Code]({row['url']})")
 
 with col4:
     for index, row in df.iterrows():
         if index % 2 == 1:
             st.header(row["title"])
+            st.write(row["description"])
+            st.image("images/" + row["image"])
+            st.write(f"[Source Code]({row['url']})")
+
